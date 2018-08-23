eceff031aa137f9155da3cec0d53dd37e5f5e519
Jaydee <jindongp@mhand.net>
2018-08-23 16:31:36 +0900

    Added basic implementation

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,27 @@
 
 import * as grpc from 'grpc';
-import { MemberService } from './protos/member_pb_service';
+import * as protoLoader from '@grpc/proto-loader';
 
-console.log(MemberService);
-// you can do that everything you want with this.
+import { MemberService } from './protos/member_pb_service';
+import { ReqMemberGet } from './protos/member_pb';
+
+const packageDef: protoLoader.PackageDefinition = protoLoader.loadSync(__dirname + '/../protos/member.proto');
+const protoDescriptor: grpc.GrpcObject = grpc.loadPackageDefinition(packageDef);
+
+
+const memberService: grpc.ServiceDefinition<MemberService> = 
+  protoDescriptor.handstudio.member.MemberService.service;
+
+const server = new grpc.Server();
+server.addService(memberService, {
+  GetMember: function(call: grpc.Call) {
+    
+  },
+
+  CreateMember: function(call: grpc.Call) {
+
+  }
+});
+
+server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+server.start();
